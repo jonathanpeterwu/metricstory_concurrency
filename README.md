dc2c6d37ca432ff48bddd8f5a4414fb2ba3862b4
jonathanpeterwu <jpwu03@yahoo.com>
2016-01-23 13:57:49 -0800

    initial skeleton

---
+++ README.md
@@ -0,0 +1,24 @@
+# Metric Story Concurrency Server
+
+@jonathanpeterwu
+
+[Project Spec Doc](https://paper.dropbox.com/doc/Metric-Story-Concurrent-API-Library-ZosYt4UJyctCKqMJb0N1R)
+
+### Setup Instructions
+
+1) Install dependencies
+```
+  npm install
+```
+
+2) Use this to run the localhost:3000 server
+
+```
+  npm run start
+```
+
+3) Go to your browser and load localhost:3001/api to see current timestamp
+
+```
+  
+```
---
+++ app.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var moment = require('moment');
+var app = express();
+var router = express.Router();
+
+// Set port
+app.set('port', 3001);
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api', router.get('/api', function(req, res, next) {
+  var time = moment().format('HH:MM:SS')
+  res.json({ time: time})
+}));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// development error handler
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
